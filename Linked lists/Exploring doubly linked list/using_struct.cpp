24897cf4dcd2a3c543b4207f9ba6d967a162fe94
Abhishek Acharya <abhishekacharya171@gmail.com>
2021-06-20 09:19:44 +0545

    Added pop function.. that can pop data at all position of list

--- Linked lists/Exploring doubly linked list/using_struct.cpp
+++ Linked lists/Exploring doubly linked list/using_struct.cpp
@@ -6,11 +6,12 @@ typedef struct Node
 	int data;
 	Node *prev;
 	Node *next;
-}Node;
+} Node;
 
 void append(Node **head, int given_data);
-void display(Node *head);
-Node* createNode(int given_data);
+void display(Node **head);
+Node *createNode(int given_data);
+void pop(Node **head, int given_data);
 
 int main()
 {
@@ -20,12 +21,63 @@ int main()
 	append(&head, 6);
 	append(&head, 7);
 	append(&head, 8);
-	
-	display(head);
+	pop(&head, 8);
+	display(&head);
 
 	return 0;
 }
-Node* createNode(int given_data)
+void pop(Node **head, int given_data)
+{
+
+	// checking whether the list is empty or not..
+	if (*head == NULL)
+	{
+		cout << "The list is empty.. " << endl;
+		return;
+	}
+
+	// so the list is not empty.. now we start transversing the list
+	Node *traveller = *head;
+
+	// navigating the element to be popped
+	while (traveller->data != given_data)
+	{
+
+		// if traveller is in last node and data is not still found.. invalid message is shown
+		if (traveller->next == NULL)
+		{
+			cout << "Sorry the data is invlid!!!" << endl;
+			return;
+		}
+
+		// since traveller is not in last node.. we push traveller forward
+		traveller = traveller->next;
+
+	}
+
+	// if the elements to be popped is in 1st position
+	if(traveller->prev == NULL)
+	{
+		traveller->next->prev = *head;
+		*head = traveller->next;
+		free(traveller);
+		return;	
+	}
+
+
+	// if the element to be popped is in last position
+	if(traveller->next == NULL)
+	{
+		traveller->prev->next = NULL;
+		free(traveller);
+		return;
+	}
+	traveller->prev->next = traveller->next;
+	traveller->next->prev = traveller->prev;
+	free(traveller);
+}
+
+Node *createNode(int given_data)
 {
 	Node *new_node = new Node;
 
@@ -35,13 +87,13 @@ Node* createNode(int given_data)
 
 	return new_node;
 }
-void display(Node* head)
+void display(Node **head)
 {
-	Node *traveller = head;
+	Node *traveller = *head;
 
 	while (traveller != NULL)
 	{
-		cout << traveller << "||" << traveller->prev << "|" << traveller->data << "|" << traveller->next << "||" << endl;
+		cout << traveller << "||" << traveller->prev << "|--> " << traveller->data << " <--|" << traveller->next << "||" << endl;
 		traveller = traveller->next;
 	}
 }
