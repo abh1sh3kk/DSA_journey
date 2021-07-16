6c5bd1ea04d7f3a4dbf81050995fc862be30c69f
Abhishek Acharya <abhishekacharya171@gmail.com>
2021-07-16 16:53:51 +0545

    insertion and printing of BST is done

--- Binary Tree/Basic tree exploration/main.cpp
+++ Binary Tree/Basic tree exploration/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class Node
+
+typedef struct Node
 {
-private:
 	int data;
 	Node *left;
 	Node *right;
+}Node;
 
-public:
-	Node *root = NULL;
-	Node *createNode(int given_data);
-	void insert(int given_data);
-	void printInOrder(Node *root);
-};
-void Node::printInOrder(Node* root)
+
+Node *createNode(int given_data);
+Node *insert(Node *root, int given_data);
+void printInOrder(Node *root);
+
+
+int main()
+{
+	Node* root = NULL;
+
+	root = insert(root, 10);
+	root = insert(root, 45);
+	root = insert(root, 34);
+	root = insert(root, 12);
+	root = insert(root, 56);
+	root = insert(root, 27);
+	root = insert(root, 10);
+
+	printInOrder(root);
+	
+	return 0;
+}
+void printInOrder(Node *root)
 {
 	if (root == NULL)
 		return;
@@ -22,34 +39,27 @@ void Node::printInOrder(Node* root)
 	cout << root->data << " ";
 	printInOrder(root->right);
 }
-void Node::insert(int given_data)
+Node* insert(Node* root, int given_data)
 {
-	//preserve the root
-	Node *temp = root;
-
-	// if root doesn't exist, create one
 	if (root == NULL)
 	{
-		Node* new_node = createNode(given_data);
-		root = new_node;
-		return;
-	}
+		// Node *new_node = createNode(given_data);
+		// root = new_node;
+		// return root;
 
-	// if given data is less than root data, change the value of root to root->left
-	if (given_data < root->data)
+		return createNode(given_data);
+	}
+	else if (given_data <= root->data)
 	{
-		root = root->left;
+		root->left = insert(root->left, given_data);
 	}
-
-	// if given data is more than root data, change the value of root to root->right
 	else
 	{
-		root = root->right;
+		root->right = insert(root->right, given_data);
 	}
-	insert(given_data);
-	root = temp;
+	return root;
 }
-Node *Node::createNode(int given_data)
+Node *createNode(int given_data)
 {
 	Node *new_node = new Node;
 
@@ -58,12 +68,4 @@ Node *Node::createNode(int given_data)
 	new_node->right = NULL;
 
 	return new_node;
-}
-int main()
-{
-	Node n1;
-
-	n1.insert(10);
-	n1.insert(45);
-	n1.printInOrder(n1.root);
 }
